2ccccf51d544fb18fd80c2c87e131ea2f9ad99d3
black <jaberhc2002@gmail.com>
2022-12-25 23:01:08 +0600

    learned Es16.Nested_object_destructuring.js

---
+++ es6/Es16.Nested_object_destructuring.js
@@ -0,0 +1,14 @@
+const studentInfo = {
+  id : 101,
+  name : "A big boss from immigration",
+  gpa : 5.00,
+  language : {
+    native : "Bangla",
+    beginners : "English"
+  }
+};
+
+const {id,gpa,name,language} = studentInfo;
+
+console.log (name,language);
+console.log (name,language.native);
