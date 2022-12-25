5ebd80724f4da8926391622e4d1cd00448280f47
black <jaberhc2002@gmail.com>
2022-12-25 22:54:05 +0600

    learned Es15.Object_destructuring.js

---
+++ es6/Es14.swap_variable.js
@@ -0,0 +1,11 @@
+// swap between to variable 
+
+var a = 10;
+var b = 20;
+
+[a,b] = [b,a];
+
+console.log (a);
+console.log (b);
+
+// thats it ;) very easy !!
---
+++ es6/Es15.Object_destructuring.js
@@ -0,0 +1,13 @@
+// Object  destructuring 
+
+const studentInfo = {
+  id : 101,
+  fullName : "Jaber chowdhury",
+  gpa : 5.00
+};
+
+const {id,fullName,gpa} = studentInfo;
+
+console.log (id,fullName,gpa);
+
+
