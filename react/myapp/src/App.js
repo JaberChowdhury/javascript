56210d28a4584047f92c76792897c6dc14e4cab9
black <jaberhc2002@gmail.com>
2022-12-26 00:06:02 +0600

    Add react project

---
+++ react/myapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from "./components/Card/Card.jsx";
+import Data from "./Data.json"
+
+function App() {
+  var items = [];
+  for (var i = 0; i < Data.length; i++) {
+    items.push(<Card name={Data[i].name} age={Data[i].age} skillStatus={Data[i].skillStatus} />);
+  }
+  return (
+    <>
+    {items}
+    </>
+  );
+}
+
+export default App;
