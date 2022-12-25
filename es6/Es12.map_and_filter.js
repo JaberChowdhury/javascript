9d7e553ea432ee5edccd2c527f8b490ddb8ee3cc
black <jaberhc2002@gmail.com>
2022-12-25 13:55:38 +0600

    learned map() and filter() in es6

---
+++ es6/Es12.map_and_filter.js
@@ -0,0 +1,23 @@
+const Mainarray = [181, 7, 8, 6, 7, 5, 7, 6, 86, 86, 86, 3, 18, 67, 5, 76, 7, 6, 8];
+
+/*
+var power = [];
+Mainarray.forEach((value)=>{
+  power.push(value*value);
+});
+*/
+// console.log (power);
+
+// let's use es6 feature 
+const power = Mainarray.map((value) => {
+    return value * value;
+});
+console.log(power);
+
+
+// filter 
+
+const less = Mainarray.filter((value) => {
+    return value > 10;
+});
+console.log(less);
