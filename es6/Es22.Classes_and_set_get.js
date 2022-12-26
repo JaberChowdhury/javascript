2cc2bd78f53d58861fcab4c95f30174fb09ada20
black <jaberhc2002@gmail.com>
2022-12-26 13:45:07 +0600

    learned Es22.Classes_and_set_get.js 😷😷😷 feeling too hard

---
+++ es6/Es22.Classes_and_set_get.js
@@ -0,0 +1,21 @@
+// es6 classes 
+
+
+class student {
+  
+  constructor(id,name){
+    this.id = id;
+    this.name = name;
+  }
+  
+  set studentname(name){
+    this.name = name;
+  }
+  get studentInfo(){
+    return this.id + " " + this.name;
+  }
+}
+
+const s1 = new student(101,"Black");
+console.log (s1); // return a object
+console.log (s1.studentInfo);
