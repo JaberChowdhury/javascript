cca0761f145225275770d8d7872d7c2944221835
black <jaberhc2002@gmail.com>
2022-12-26 13:30:14 +0600

    learned Es20.Export_a_module.js Es21.Import_a_module.js

---
+++ es6/Es20.Export_a_module.js
@@ -0,0 +1,3 @@
+const text = "Hi there ,I am a modules and exported from Es20.Export_a_module.js";
+
+export text ;
---
+++ es6/Es21.Import_a_module.js
@@ -0,0 +1,5 @@
+import text from "./Es20.Export_a_module.js";
+
+document.write(text);
+
+console.log(text);
