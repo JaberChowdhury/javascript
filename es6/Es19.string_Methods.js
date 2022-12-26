e98620aad16a3ac7f64e560afe67a22af20d3555
black <jaberhc2002@gmail.com>
2022-12-26 13:18:57 +0600

    learned Es19.string_Methods.js

---
+++ es6/Es19.string_Methods.js
@@ -0,0 +1,19 @@
+// string methods
+
+const string = "I live in Bangladesh";
+
+// startsWith
+console.log (string.startsWith("live",2));
+
+console.log (string.endsWith("Bangladesh"));
+
+console.log (string.includes("in"));
+
+// Remember that these methods are case sensitive 
+/*
+thes3 methods always return true or false
+
+
+structure 
+string.methods("searching_word",position_number)
+*/
