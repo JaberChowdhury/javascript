67e104cd3fd542ca3211ec757f84a61992ddc841
black <jaberhc2002@gmail.com>
2022-12-26 11:47:26 +0600

    codded on react

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Card from "./components/Card/Card.jsx";
-import Data from "./Data.json"
+import Data from "./Data.json";
 
-function App() {
+
+const App=()=>{ 
   var items = [];
   for (var i = 0; i < Data.length; i++) {
-    items.push(<Card name={Data[i].name} age={Data[i].age} skillStatus={Data[i].skillStatus} />);
+    items.push(
+    <Card 
+    name={Data[i].name} 
+    age={Data[i].age} 
+    skillStatus={Data[i].skillStatus} 
+    />);
   }
+
   return (
     <>
     {items}
