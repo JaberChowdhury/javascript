61832711a52cf27807ed293ecedc7667ddbfb30f
black <jaberhc2002@gmail.com>
2023-01-03 17:02:17 +0600

    dont know what i have done

--- es6/Es09.Default_and_rest_parameter.js
+++ es6/Es09.Default_and_rest_parameter.js
@@ -17,6 +17,6 @@ and sent that empty message so we add a default value of our user message
 
 const getNumber = (x, y, ...z) => {
     console.log(`${x}, ${y}, this is z value= ${z}`);
-    console.log(typeof(z)); // object
+    console.log(typeof(z)); // object actually an array
 };
 getNumber(16, 57, 57, 67, 7, 6, 6, 66, 6, 79);
