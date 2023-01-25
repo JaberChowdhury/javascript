cc4b2b746d6c44df87cf9b81a7df83779aebe96d
black <jaberhc2002@gmail.com>
2023-01-25 11:19:11 +0600

    Learned js_basic/JS_Web_APIs/wa03.js_fetch.js js_basic/JS_Web_APIs/wa04.fetch_from_api.h and add a project mini_project/Rotating_animation.html

---
+++ js_basic/JS_Web_APIs/wa03.js_fetch.js
@@ -0,0 +1,19 @@
+// basic structure
+/*
+
+async function getText(file) {
+  let x = await fetch(file);
+  let y = await x.text();
+  myDisplay(y);
+}
+
+*/
+
+async function getinfo() {
+  let data = await fetch(
+    "https://mocki.io/v1/1af210dc-c1ba-46fb-b74f-ede5695156f0"
+  );
+  let json = await data.json();
+  console.log([...json].length);
+}
+getinfo();
