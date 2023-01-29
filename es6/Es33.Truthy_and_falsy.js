598d006d320bc71f59b579824810877ba6ed1ddb
black <jaberhc2002@gmail.com>
2023-01-30 01:09:26 +0600

    learned Es33.Truthy_and_falsy.js

---
+++ es6/Es33.Truthy_and_falsy.js
@@ -0,0 +1,15 @@
+let value = "Ok";
+
+if(value) {
+  console.log("This is truthy");
+} else {
+  console.log("This is falsy");
+}
+// This will return true
+
+
+/*
+So whaich value is false?
+
+false , null, NaN, undefined, 0, ""
+*/
