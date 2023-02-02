a16fd26a55e4194871ce15a99d94414d26a61c39
black <jaberhc2002@gmail.com>
2023-02-02 19:53:51 +0600

    Creating midjourney dataset visiual in website ,some work is good somthing is unnecessery :)

---
+++ test/index.js
@@ -0,0 +1,20 @@
+const getData = async () => {
+  const data = await fetch("http://localhost:3000");
+  const jsondata = await data.json();
+  const final = jsondata.messages;
+
+  final.map((value, index = 3) => {
+    try {
+      const { id, type, content } = final[index][0]; //Done
+      const { username, avatar, avatar_decoration, public_flags, bot } =
+        final[index][0].author; //Done
+      const { size, url, proxy_url, width, height, content_type } =
+        final[index][0].attachments[0]; //Done
+    } catch (error) {
+      console.log(error.name);
+      console.log(index);
+    }
+  });
+};
+
+getData();
--- test/jsonfiles/Api.js
+++ test/jsonfiles/Api.js
@@ -1,9 +1,10 @@
 async function getData() {
   //let req = await fetch("https://jsonplaceholder.typicode.com/posts");
-  let req = await fetch("https://mocki.io/v1/ea4dd910-ad95-4558-be79-d00be239230a");
+  let req = await fetch(
+    "https://mocki.io/v1/ea4dd910-ad95-4558-be79-d00be239230a"
+  );
   let data = await req.json();
   let da = data.messages;
-  console.log (da[0])
-  
+  console.log(da[0]);
 }
 getData();
