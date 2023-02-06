25f1810fa68bd40c91c007fe94b152871bcd15d9
black <jaberhc2002@gmail.com>
2023-02-07 00:33:58 +0600

    Added a practise in es6 folder about Object concatination 😊 and beautified 😑

--- es6/Es35.Object_concatination.js
+++ es6/Es35.Object_concatination.js
@@ -1,33 +1,27 @@
-const object1 = { name1 : "object1", define1 : "It's a very good object" };
-const object2 = { name2 : "object2", define2 : "It's a very good object" };
-const object3 = { name3 : "object3", define3 : "It's a very good object" };
+const object1 = { name1: "object1", define1: "It's a very good object" };
+const object2 = { name2: "object2", define2: "It's a very good object" };
+const object3 = { name3: "object3", define3: "It's a very good object" };
 
-
-try{
-  const final = {...object1,...object2,...object3};
-  console.log (final);
-}catch(e){
-  console.log (e);
+try {
+  const final = { ...object1, ...object2, ...object3 };
+  console.log(final);
+} catch (e) {
+  console.log(e);
 }
-console.log ("everything is ok"); // :)
-
+console.log("everything is ok"); // :)
 
 /*
 But if the property of these objects are same than spread operator will overlape the property's value
 */
 
+const object4 = { name: "object4", define: "It's a very good object" };
+const object5 = { name: "object5", define: "It's a very good object" };
+const object6 = { name: "object6", define: "It's a very good object" };
 
-
-const object4 = { name : "object4", define : "It's a very good object" };
-const object5 = { name : "object5", define : "It's a very good object" };
-const object6 = { name : "object6", define : "It's a very good object" };
-
-
-try{
-  const final2 = {...object4,...object5,...object6};
-  console.log (final2);
-}catch(e){
-  console.log (e);
+try {
+  const final2 = { ...object4, ...object5, ...object6 };
+  console.log(final2);
+} catch (e) {
+  console.log(e);
 }
 console.table("everything is ok but with some difference"); // :)
-
