57ae386f6610144962c529db3c7413514cea75d5
black <jaberhc2002@gmail.com>
2023-02-12 11:13:41 +0600

    learned es6/Es42.Array_reduce_method.js

---
+++ es6/Es42.Array_reduce_method.js
@@ -0,0 +1,21 @@
+/*
+Array.__proto__.reduce() 
+this is one of the most important array method 
+
+📛 array.__proto__.reduce() don't change the main array
+   it's create a new value
+   
+   
+   Array.reduce((prevValue,currentValue,currentIndex,array)=>{
+     return something;
+   }, initial)
+   
+*/
+
+const num = [..."0123456789"];
+
+const sum = num.reduce((prev,current,currentindex,arr)=>{
+  return parseInt(prev) + parseInt(current);
+},0);
+
+console.log (sum);
