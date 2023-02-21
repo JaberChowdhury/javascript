90694da9a51c316cb4ae0e2d2ba4aad0f86d908a
black <jaberhc2002@gmail.com>
2023-02-21 18:27:04 +0600

    add some items to learn chartjs

--- lab/index.js
+++ lab/index.js
@@ -1,16 +1,35 @@
-const container = document.querySelectorAll(".container")[0];
-
-const rgb = () => {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  return `rgb(${r},${g},${b})`;
+var getRandomDataArray = function () {
+  var dataArray = [];
+  for (var i = 0; i <= 90; i++) dataArray.push(Math.tan(i));
+  return dataArray;
 };
 
-let arr = "";
-for (let i = 0; i < 360; i++) {
-  let x = Math.tan(i);
-  arr += `<div class="w-[${x}px] h-[${x}px] p-2 rounded-full bg-[${rgb()}] bg-rose-500"></div> `;
-}
+let getLabel = () => {
+  var arr = [];
+  for (let i = 0; i <= 90; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
 
-container.innerHTML = arr;
+var chartData = {
+  labels: getLabel(),
+  datasets: [
+    {
+      label: "My First dataset",
+      fillColor: "rgba(22,22,22,0.2)",
+      strokeColor: "rgba(20,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#00000",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(222,222,20,1)",
+      data: getRandomDataArray(),
+    },
+  ],
+};
+
+window.onload = function () {
+  var chartOptions = { responsive: true };
+  var chart = document.getElementById("canvas").getContext("2d");
+  window.myBar = new Chart(chart).Line(chartData, chartOptions);
+};
