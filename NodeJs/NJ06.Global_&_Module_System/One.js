f0ee3816cb486f37dc464d18dcbae38ec9833bd9
black <jaberhc2002@gmail.com>
2023-06-01 00:52:19 +0600

    🫒🫒🫒🫒🫒🫒Learned today somthing about module based file system in nodejs🫒🫒🫒🫒🫒

--- NodeJs/NJ06.Global_&_Module_System/One.js
+++ NodeJs/NJ06.Global_&_Module_System/One.js
@@ -1,25 +1,20 @@
 /**
- * 
+ *
  * Title : Module based file system
- * Description : This module contain only an array for export 
- * Author : Md Jaber Hossain Chowdhury 
+ * Description : This module contain only an array for export
+ * Author : Md Jaber Hossain Chowdhury
  * Date : 1 June 2023
- * 
-**/
+ *
+ **/
 
 // Some of my favourite food emojis !!
 
 const food = [..."🍇🍈🍉🍊🍋🍒🫒🍅🥥🥑🥕🥭🍌🍍"];
- 
+
 // Some of my favourite emojis !!
 
-const emojis = [..."😊🥰🤗🥀"]
- 
- 
- 
- 
- 
-module.exports = {food,emojis};
+const emojis = [..."😊🥰🤗🥀"];
 
+module.exports = { food, emojis };
 
- console.log (module)
+console.log(module);
