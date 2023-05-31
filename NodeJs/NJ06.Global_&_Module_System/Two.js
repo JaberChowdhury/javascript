a07e50d244ff4dc6e323aaf696a4f3432db414f4
black <jaberhc2002@gmail.com>
2023-06-01 00:54:03 +0600

    🫒🫒🫒🫒🫒🫒Sane Bro git add .

--- NodeJs/NJ06.Global_&_Module_System/Two.js
+++ NodeJs/NJ06.Global_&_Module_System/Two.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * Title : Module based file system
+ * Description : This module contain only an array for export
+ * Author : Md Jaber Hossain Chowdhury
+ * Date : 1 June 2023
+ *
+ **/
+
 const fruit = require("./One");
 
-// console.log(fruit)
+console.log(fruit);
