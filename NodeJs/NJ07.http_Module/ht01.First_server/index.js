e339e6069ca4d51c128880b10b72efcf82cd29d6
black <jaberhc2002@gmail.com>
2023-06-01 18:31:39 +0600

    Created our first server

--- NodeJs/NJ07.http_Module/ht01.First_server/index.js
+++ NodeJs/NJ07.http_Module/ht01.First_server/index.js
@@ -1,30 +1,28 @@
 /**
- * 
+ *
  * 🫒 Title : http server
  * 🫒 Description : this is a basic http server made on nodejs http module
- * 🫒 Author : Md Jaber Hossain Chowdhury 
+ * 🫒 Author : Md Jaber Hossain Chowdhury
  * 🫒 Date : 1 June 2023
- * 
-**/
+ *
+ **/
 
-// Dependencies 
+// Dependencies
 const http = require("http");
 
-// App object - module scaffolding 
+// App object - module scaffolding
 const app = {};
 
-
-// Configuration 
+// Configuration
 app.config = {
-  port : 3000
+  port: 3000,
 };
 
+// Create the server
+const server = http.createServer((req, res) => {
+  res.end("I am a server from nodejs");
+});
 
-// Create the server 
-const server = http.createServer((req,res)=>{
-  res.end("I am a server from nodejs")
-})
-
-server.listen(app.config.port,()=>{
-  console.log (`Server running at ${app.config.port}`)
+server.listen(app.config.port, () => {
+  console.log(`Server running at ${app.config.port}`);
 });
