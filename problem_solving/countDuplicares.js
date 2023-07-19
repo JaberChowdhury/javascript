da33f5f8947776e2cff813412fb4a09fe058ca42
black <jaberhc2002@gmail.com>
2023-07-19 11:27:19 +0600

    added a new problem_solving about countDuplicares.js

---
+++ problem_solving/countDuplicares.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * 🫒 Title : countDuplicates
+ * 🫒 Description : find duplicate value and add them in object
+ * 🫒 Author : Md Jaber Hossain Chowdhury
+ * 🫒 Date : 06 July 2023
+ *
+ **/
+
+const input = [..."🍇🍈🍐🫒🫒🌶🍇🍐🥕🧄🥦🍒🍒"];
+
+const countDuplicates = (data) => {
+  let output = {};
+  data.map((d) => {
+    output[d] = (output[d] || 0) + 1;
+  });
+  return output;
+};
+
+console.log(countDuplicates(input));
